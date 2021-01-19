07a72081b1aaf91a3f41c12df1c0138814e0daa8
Kristian Vassilev <kvassilev@ephedratk.com>
2021-01-19 13:21:35 +0200

    Create GreaterNumber.js
    Compares two numbers and returns the greater one.

---
+++ JavaScript(JS) Basics/LogicalStatements/GreaterNumber.js
@@ -0,0 +1,12 @@
+function greaterNumber(a, b) {
+  if (a > b) {
+    console.log(a);
+  } else {
+    console.log(b);
+  }
+
+  // ctrl + /
+  // let maxNumber = Math.max(a, b);
+  // console.log(maxNumber);
+}
+greaterNumber("-5", "5");
