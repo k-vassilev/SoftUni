7375a80d885aa614e2c773874b10fd0efe66efef
Kristian Vassilev <kvassilev@ephedratk.com>
2021-02-04 07:27:59 +0200

    Create SumOfVowels.js
    Calculates the sum of all vowels in a given word.

---
+++ JavaScript(JS) Basics/ForLoop/SumOfVowels.js
@@ -0,0 +1,27 @@
+function solve(word) {
+  let sum = 0;
+
+  for (i = 0; i < word.length; i++) {
+    let wordChar = word[i];
+
+    switch (wordChar) {
+      case `a`:
+        sum += 1;
+        break;
+      case `e`:
+        sum += 2;
+        break;
+      case `i`:
+        sum += 3;
+        break;
+      case `o`:
+        sum += 4;
+        break;
+      case `u`:
+        sum += 5;
+        break;
+    }
+  }
+  console.log(sum);
+}
+solve("beer");
