8f3351a272e36cda846d6c3e8e9fd75a9c87871b
Kristian Vassilev <kvassilev@ephedratk.com>
2021-02-11 09:00:04 +0200

    Create SpecialNumbers.js
    Outputs all possible positions of the input number within a 4-digit output.

---
+++ JavaScript(JS) Basics/NestedLoops/SpecialNumbers.js
@@ -0,0 +1,18 @@
+function solve(input) {
+  let n = Number(input[0]);
+  let output = "";
+
+  for (i = 1; i <= 9; i++) {
+    for (j = 1; j <= 9; j++) {
+      for (k = 1; k <= 9; k++) {
+        for (l = 1; l <= 9; l++) {
+          if ((n % i === 0) & (n % j === 0) & (n % k === 0) & (n % l === 0)) {
+            output += `${i}${j}${k}${l} `;
+          }
+        }
+      }
+    }
+  }
+  console.log(output);
+}
+solve(["3"]);
