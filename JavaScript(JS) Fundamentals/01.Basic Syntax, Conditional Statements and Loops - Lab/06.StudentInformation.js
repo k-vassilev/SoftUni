94dceabee8c64ed2ff8e0e51404596f840d28c99
Kristian Vassilev <kvassilev@ephedratk.com>
2021-02-22 08:54:50 +0200

    Create 06.StudentInformation.js
    Interpolates the input into a sentence.

---
+++ JavaScript(JS) Fundamentals/01.Basic Syntax, Conditional Statements and Loops - Lab/06.StudentInformation.js
@@ -0,0 +1,5 @@
+function solve(studentName, age, averageGrade) {
+  averageGrade = Number(averageGrade).toFixed(2);
+  console.log(`Name: ${studentName}, Age: ${age}, Grade: ${averageGrade}`);
+}
+solve("John", 15, 5.54678);
