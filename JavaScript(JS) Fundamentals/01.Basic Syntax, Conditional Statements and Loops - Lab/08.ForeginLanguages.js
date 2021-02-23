741dfff87039e6ce473a40dd8cc0a838de47f37a
Kristian Vassilev <kvassilev@ephedratk.com>
2021-02-23 08:27:38 +0200

    Create 08.ForeginLanguages.js
    Outputs the nationality of a person, based on the input country.

---
+++ JavaScript(JS) Fundamentals/01.Basic Syntax, Conditional Statements and Loops - Lab/08.ForeginLanguages.js
@@ -0,0 +1,14 @@
+function solve(input) {
+  if (input === `USA`) {
+    console.log(`English`);
+  } else if (input === `England`) {
+    console.log(`English`);
+  } else if (input === `Spain`) {
+    console.log(`Spanish`);
+  } else if (input === `Argentina`) {
+    console.log(`Spanish`);
+  } else if (input === `Mexico`) {
+    console.log(`Spanish`);
+  } else console.log(`unknown`);
+}
+solve(`Mexico`);
