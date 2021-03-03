e7fffb2664c35fc64040e9dd870b01eef987877d
Kristian Vassilev <kvassilev@ephedratk.com>
2021-03-03 08:50:27 +0200

    Create 02.CharsToString.js
    Apends the input chars to a string.

---
+++ JavaScript(JS) Fundamentals/02.Data Types and Variables - Exercise/02.CharsToString.js
@@ -0,0 +1,10 @@
+function solve(a, b, c) {
+  a = String(a);
+  b = String(b);
+  c = String(c);
+  let combinedString = a + b + c;
+  console.log(combinedString);
+}
+solve("a", "b", "c");
+solve("%", "2", "o");
+solve("1", "5", "p");
