ffe573e83bc89d95ef7526b9e7832a0994ab2bec
Kristian Vassilev <kvassilev@ephedratk.com>
2021-03-11 11:22:52 +0200

    Create 05.MaxNumber.js
    Outputs the top integers in an array. A top integer is an integer which is bigger than all the elements to its right.

---
+++ JavaScript(JS) Fundamentals/03.Arrays - Exercise/05.MaxNumber.js
@@ -0,0 +1,20 @@
+function solve(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let bigger = true;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] <= arr[j]) {
+        bigger = false;
+      }
+    }
+    if (bigger) {
+      newArr.push(arr[i]);
+    }
+  }
+  console.log(newArr.join(` `));
+}
+solve([1, 4, 3, 2]);
+solve([14, 24, 3, 19, 15, 17]);
+solve([41, 41, 34, 20]);
+solve([27, 19, 42, 2, 13, 45, 48]);
