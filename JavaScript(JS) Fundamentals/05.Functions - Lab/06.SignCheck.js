bbdfb94e68db5a8d41e76158c0acd6dd960de666
Kristian Vassilev <kvassilev@ephedratk.com>
2021-03-14 10:23:06 +0200

    Create 06.SignCheck.js
    Checks what the sign of all numbers in an array  is.

---
+++ JavaScript(JS) Fundamentals/05.Functions - Lab/06.SignCheck.js
@@ -0,0 +1,17 @@
+function signCheck(numOne, numTwo, numThree) {
+  if (numOne == 0 || numTwo == 0 || numThree == 0) {
+    console.log(`Positive`);
+  } else if (numOne > 0 && numTwo > 0 && numThree > 0) {
+    console.log(`Positive`);
+  } else if (numOne < 0 && numTwo < 0 && numThree > 0) {
+    console.log(`Positive`);
+  } else if (numOne > 0 && numTwo < 0 && numThree < 0) {
+    console.log(`Positive`);
+  } else {
+    console.log(`Negative`);
+  }
+}
+signCheck(5, 12, -15);
+signCheck(-6, -12, 14);
+signCheck(-1, -2, -3);
+signCheck(-1, 0, 1);
