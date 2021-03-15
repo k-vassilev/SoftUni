619a0e84448febede5259ab00d68c3aff1d05d50
Kristian Vassilev <kvassilev@ephedratk.com>
2021-03-15 10:27:26 +0200

    Create 02.AddsSubtract.js
    Adds the first two nums and then subtracts the third.

---
+++ JavaScript(JS) Fundamentals/05.Functions - Exercise/02.AddsSubtract.js
@@ -0,0 +1,6 @@
+function addAndSubstract(firstNum, secondNum, thirdNum) {
+  let addSum = firstNum + secondNum;
+  let result = addSum - thirdNum;
+  console.log(result);
+}
+addAndSubstract(23, 6, 10);
