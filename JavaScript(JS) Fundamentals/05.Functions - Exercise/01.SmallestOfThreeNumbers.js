eb6eb164d7e1ab3c309b5ceefe05c9c8e701f1d0
Kristian Vassilev <kvassilev@ephedratk.com>
2021-03-15 10:26:33 +0200

    Create 01.SmallestOfThreeNumbers.js
    Outputs the smallest of three numbers.

---
+++ JavaScript(JS) Fundamentals/05.Functions - Exercise/01.SmallestOfThreeNumbers.js
@@ -0,0 +1,5 @@
+function smallestOfThreeNumbers(firstNum, secondNum, thirdNum) {
+  let result = Math.min(firstNum, secondNum, thirdNum);
+  console.log(result);
+}
+smallestOfThreeNumbers(2, 5, 3);
