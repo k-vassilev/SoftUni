94162d0a79866bef6d93f582f1a7aa2b4de2a793
Kristian Vassilev <kvassilev@ephedratk.com>
2021-03-18 09:53:45 +0200

    Create 01.SumFirstAndLast.js
    Sums the first nd last elements of an array.

---
+++ JavaScript(JS) Fundamentals/06.Arrays Advanced - Lab/01.SumFirstAndLast.js
@@ -0,0 +1,7 @@
+function sumFirstLast(arr) {
+  let first = Number(arr[0]);
+  let last = Number(arr[arr.length - 1]);
+  console.log(first + last);
+}
+sumFirstLast(["20", "30", "40"]);
+sumFirstLast(["5", "10"]);
