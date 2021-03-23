f7c41e59154cf0399ad7589d6a4048d6e06ed201
Kristian Vassilev <kvassilev@ephedratk.com>
2021-03-23 10:44:08 +0200

    Create 01.PersonInfo.js
    A function that receives 3 parameters, sets them to an object and returns that object

---
+++ JavaScript(JS) Fundamentals/07.Objects and Classes - Lab/01.PersonInfo.js
@@ -0,0 +1,9 @@
+function personInfo(firstName, secondName, age) {
+  let person = {};
+  person.firstName = firstName;
+  person.lastName = secondName;
+  person.age = age;
+  return person;
+}
+
+personInfo("Peter", "Pan", "20");
