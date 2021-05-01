1c83d12673fa8dddc8c35934c42ac9edacd2d800
Kristian Vassilev <kvassilev@ephedratk.com>
2021-05-01 15:27:01 +0300

    Create ShowMore.js

---
+++ JavaScript(JS) Advanced/04.DOM Introduction - Lab/04.ShowMore/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+  let element = document.getElementById(`text`);
+  element.style.display = `inline`;
+  let toShow = document.getElementById(`more`);
+  toShow.style.display = `none`;
+}
