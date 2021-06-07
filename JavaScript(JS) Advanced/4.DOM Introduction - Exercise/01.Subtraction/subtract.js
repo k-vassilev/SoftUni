f92189104ef1b1b8d334f6739ed827cc29e3355b
Kristian Vassilev <kvassilev@ephedratk.com>
2021-06-07 05:57:34 +0300

    Delete JavaScript(JS) Advanced/4.DOM Introduction - Exercise directory

--- JavaScript(JS) Advanced/4.DOM Introduction - Exercise/01.Subtraction/subtract.js
+++
@@ -1,7 +0,0 @@
-function subtract() {
-  let firstNum = Number(document.querySelector("#firstNumber").value);
-  let secondNum = Number(document.querySelector("#secondNumber").value);
-  let result = firstNum - secondNum;
-  let resultField = document.querySelector("#result"); // gets the actual div, not the content of it
-  resultField.textContent = result; // changes the value of the above field
-}
--- JavaScript(JS) Advanced/4.DOM Introduction - Exercise/03.Accordion/accordion.js
+++
@@ -1,7 +0,0 @@
-function toggle() {
-  console.log("TODO:...");
-  let showMore = document.querySelector(`#extra`);
-  let button = document.querySelector(`.button`);
-  showMore.style.display = showMore.style.display !== `block` ? `block` : `none`;
-  button.textContent = button.textContent !== `More` ? `More` : `Less`;
-}
