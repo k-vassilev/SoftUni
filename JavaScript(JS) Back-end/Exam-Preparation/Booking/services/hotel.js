b5ce2507e8944ed52f1ba608ecef1a68baf0a401
Kristian Vassilev <kvassilev@ephedratk.com>
2021-06-30 08:39:32 +0300

    Add files via upload

---
+++ JavaScript(JS) Back-end/Exam-Preparation/Booking/services/hotel.js
@@ -0,0 +1,54 @@
+const Hotel = require('../models/Hotel');
+const User = require('../models/User');
+
+async function createHotel(hotelData){
+    const hotel = new Hotel(hotelData);
+    await hotel.save();
+
+    return hotel;
+}
+
+async function getAllHotels(){
+    const hotels = await Hotel.find({}).lean();
+
+    return hotels;
+}
+
+async function getHotelById(id){
+    const hotel = await Hotel.findById(id).lean();
+
+    return hotel;
+}
+
+async function editHotel(id, hotelData){
+    const hotel = await Hotel.findById(id);
+
+    hotel.name = hotelData.name;
+    hotel.city = hotelData.city;
+    hotel.rooms = Number(hotelData.rooms);
+    hotel.imageUrl = hotelData.imageUrl;
+
+    return hotel.save();
+}
+
+async function bookHotel(hotelId, userId){
+    const hotel = await Hotel.findById(hotelId);
+    const user = await User.findById(userId);
+
+    if (user._id == hotel.owner){
+        throw new Error ('Cannot book hotel you have created!')
+      }
+
+    user.bookedHotels.push(hotelId);
+    hotel.bookedBy.push(user);
+
+    return Promise.all([user.save(), hotel.save()]);
+}
+
+module.exports = {
+    createHotel,
+    getAllHotels,
+    getHotelById,
+    editHotel,
+    bookHotel
+};
---
+++ JavaScript(JS) Back-end/Exam-Preparation/Booking/services/user.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+
+async function createUser(username, email, hashedPassword) {
+  // TODO adapt properties to project requirements
+
+  const user = new User({
+    username,
+    email,
+    hashedPassword,
+  });
+
+  await user.save();
+
+  return user;
+}
+
+async function getUserByUsername(username) {
+  const pattern = new RegExp(`^${username}$`, "i");
+  const user = await User.findOne({ username: { $regex: pattern } });
+  return user;
+}
+
+async function getUserByEmail(email) {
+  const pattern = new RegExp(`^${email}$`, "i");
+  const user = await User.findOne({ email: { $regex: pattern } });
+  return user;
+}
+
+// TODO add functions for finding user by other properties,
+// as specified in the project requirements (like e-mails)
+
+module.exports = {
+  createUser,
+  getUserByUsername,
+  getUserByEmail
+};
