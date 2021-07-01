b8f8b7adf600ee78c4d65d1ac04d18e97814fec3
Kristian Vassilev <kvassilev@ephedratk.com>
2021-07-01 09:09:29 +0300

    Add files via upload

---
+++ JavaScript(JS) Back-end/Exam-Preparation/Theater/util/parsers.js
@@ -0,0 +1,10 @@
+function parseError(err){
+    if(err.name == 'ValidationError'){
+        return Object.values(err.errors).map(e => e.properties.message)
+    }else{
+        return [err.message];
+    }
+};
+module.exports = {
+    parseError
+};
