96359c943a6c2adb729986e0e7b00a8da223292d
Kristian Vassilev <kvassilev@ephedratk.com>
2021-07-01 09:10:02 +0300

    Add files via upload

---
+++ JavaScript(JS) Back-end/Exam-Preparation/Theater/services/play.js
@@ -0,0 +1,54 @@
+const Play = require("../models/Play");
+
+async function getAllPlays() {
+  return Play.find({ public: true }).sort({ createdAt: -1 }).lean();
+}
+
+async function getPlayById(id) {
+    return Play.findById(id).populate('usersLiked').lean();
+}
+
+async function createPlay(playData) {
+  const pattern = new RegExp(`^${playData.title}$`, "i");
+  const existing = await Play.findOne({ title: { $regex: pattern } });
+
+  if (existing) {
+    throw new Error("A play with this name already exists!");
+  }
+  const play = new Play(playData);
+  await play.save();
+
+  return play;
+}
+
+async function editPLay(id, playData) {
+    const play = await Play.findById(id);
+
+    play.title = playData.title;
+    play.description = playData.description;
+    play.imageUrl = playData.imageUrl;
+    play.public = Boolean(playData.public);
+
+    return play.save();
+}
+
+async function deletePlay(id) {
+    return Play.findByIdAndDelete(id);
+}
+
+async function likePlay(playId, userId){
+    const play = await Play.findById(playId);
+
+    play.usersLiked.push(userId);
+  
+    return play.save();
+}
+
+module.exports = {
+  getAllPlays,
+  getPlayById,
+  createPlay,
+  editPLay,
+  deletePlay,
+  likePlay,
+};
