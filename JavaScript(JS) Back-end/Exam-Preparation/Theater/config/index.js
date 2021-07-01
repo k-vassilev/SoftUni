f72a0c8cc415c0a5ee5a48ffa1cb4e8797d19d9f
Kristian Vassilev <kvassilev@ephedratk.com>
2021-07-01 09:11:07 +0300

    Add files via upload

---
+++ JavaScript(JS) Back-end/Exam-Preparation/Theater/config/index.js
@@ -0,0 +1,8 @@
+//Change 'skeleton-db' to current project name db
+
+module.exports = {
+    PORT: 3000,
+    DB_CONNECTION_STRING: 'mongodb://localhost:27017/theaters',
+    TOKEN_SECRET: 'The very best security the world has to offer',
+    COOKIE_NAME: 'SESSION_TOKEN'
+};
